dcb54fa8e68d8fbf4943468612b899704d43908e
Lanan <lananwu@gmail.com>
2023-03-20 20:38:24 -0700

    Changed tasks names according to date in calendar

--- index.js
+++ index.js
@@ -1,124 +1,216 @@
-		let tasks = [];
+//important variables
+let fullDate = new Date(Date.now());
+let pstDate = fullDate.toLocaleString("en-US", {
+  timeZone: "America/Los_Angeles",
+});
+pstDate = new Date(pstDate);
+let year = pstDate.getFullYear();
+let month = pstDate.getMonth() + 1;
+let day = pstDate.getDate();
+let closestThreeDays = getClosestThreeDays();
 
-		// check if tasks are stored in local storage
-		if (localStorage.getItem("tasks")) {
-			tasks = JSON.parse(localStorage.getItem("tasks"));
-		}
+//start
+let tasks = [];
 
-		// function to render tasks in the table
-		function renderTasks() {
-			let tableBody = document.querySelector("#tasks-table tbody");
-			tableBody.innerHTML = "";
-			for (let i = 0; i < tasks.length; i++) {
-				let row = tableBody.insertRow();
-				let taskCell = row.insertCell();
-				let doneCell = row.insertCell();
-				let oneCell = row.insertCell();
-				let twoCell = row.insertCell();
-				let deleteCell = row.insertCell();
-				taskCell.innerHTML = tasks[i].task;
-				doneCell.innerHTML = "<input type='checkbox' class='task-done-today' data-index='" + i + "'" + (tasks[i].done ? " checked" : "") + ">";
-				oneCell.innerHTML = "<input type='checkbox' class='task-done-1day' data-index='" + i + "'" + (tasks[i].done1 ? " checked" : "") + ">";
-				twoCell.innerHTML = "<input type='checkbox' class='task-done-2day' data-index='" + i + "'" + (tasks[i].done2 ? " checked" : "") + ">";
-				deleteCell.innerHTML = "<button class='delete-button' data-index='" + i + "'>Delete</button>";
-			}
-		}
+// check if tasks are stored in local storage
+if (localStorage.getItem("tasks")) {
+  tasks = JSON.parse(localStorage.getItem("tasks"));
+}
 
-		// render tasks on page load
-		renderTasks();
+// render tasks on page load
+renderTasks();
 
-		// event listener for add task form submit
-		document.querySelector("#add-task-form").addEventListener("submit", function(event) {
-			event.preventDefault();
-			let taskInput = document.querySelector("#task-input");
-			let task = taskInput.value.trim();
-			if (task) {
-				let newTask = {
-					task: task,
-					done: false,
-                    done1: false,
-                    done2: false,
-				};
-				tasks.push(newTask);
-				localStorage.setItem("tasks", JSON.stringify(tasks));
-				renderTasks();
-				taskInput.value = "";
-			}
-		});
+//function to reset all the variables
+function resetVariables() {
+  pstDate = fullDate.toLocaleString("en-US", {
+    timeZone: "America/Los_Angeles",
+  });
+  pstDate = new Date(pstDate);
+  year = pstDate.getFullYear();
+  month = pstDate.getMonth() + 1;
+  day = pstDate.getDate();
+}
 
-		// event listener for task done checkbox click - today
-		document.addEventListener("change", function(event) {
-			if (event.target.matches(".task-done-today")) {
-				let index = event.target.dataset.index;
-				tasks[index].done = event.target.checked;
-				localStorage.setItem("tasks", JSON.stringify(tasks));
-			}
-		});
+//function to get yesterday date
+function dayMinusOne() {
+  pstDate.setDate(new Date(Date.now()).getDate() - 1);
+  year = pstDate.getFullYear();
+  month = pstDate.getMonth() + 1;
+  day = pstDate.getDate();
+}
 
-		// event listener for task done checkbox click - 1 day ago
-        document.addEventListener("change", function(event) {
-			if (event.target.matches(".task-done-1day")) {
-				let index = event.target.dataset.index;
-				tasks[index].done1 = event.target.checked;
-				localStorage.setItem("tasks", JSON.stringify(tasks));
-			}
-		});
+function getClosestThreeDays() {
+  let today = year + "/" + month + "/" + day;
+  if (day == 1) {
+    dayMinusOne();
+  } else {
+    day -= 1;
+  }
+  let oneDayAgo = year + "/" + month + "/" + day;
+  if (day == 1) {
+    dayMinusOne();
+  } else {
+    day -= 1;
+  }
+  let twoDaysAgo = year + "/" + month + "/" + day;
+  resetVariables();
+  return [today, oneDayAgo, twoDaysAgo];
+}
 
-        // event listener for task done checkbox click - 1 day ago
-        document.addEventListener("change", function(event) {
-			if (event.target.matches(".task-done-2day")) {
-				let index = event.target.dataset.index;
-				tasks[index].done2 = event.target.checked;
-				localStorage.setItem("tasks", JSON.stringify(tasks));
-			}
-		});
+// event listener for add task form submit
+document
+  .querySelector("#add-task-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    let taskInput = document.querySelector("#task-input");
+    let task = taskInput.value.trim();
+    if (task) {
+      let newTask = {
+        task: task,
+        [closestThreeDays[0]]: false,
+        [closestThreeDays[1]]: false,
+        [closestThreeDays[2]]: false,
+      };
+      tasks.push(newTask);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      renderTasks();
+      taskInput.value = "";
+      resetVariables();
+    }
+  });
 
-		// event listener for delete button click
-		document.addEventListener("click", function(event) {
-			if (event.target.matches(".delete-button")) {
-				let index = event.target.dataset.index;
-				tasks.splice(index, 1);
-				localStorage.setItem("tasks", JSON.stringify(tasks));
-				renderTasks();
-			}
-		});
+// function to render tasks in the table
+function renderTasks() {
+  let tableBody = document.querySelector("#tasks-table tbody");
+  tableBody.innerHTML = "";
+  for (let i = 0; i < tasks.length; i++) {
+    let row = tableBody.insertRow();
+    let taskCell = row.insertCell();
+    let doneCell = row.insertCell();
+    let oneCell = row.insertCell();
+    let twoCell = row.insertCell();
+    let deleteCell = row.insertCell();
+    taskCell.innerHTML = tasks[i].task;
+    doneCell.innerHTML =
+      "<input type='checkbox' class='task-done-today' data-index='" +
+      i +
+      "'" +
+      (tasks[i][closestThreeDays[0]] ? " checked" : "") +
+      ">";
+    oneCell.innerHTML =
+      "<input type='checkbox' class='task-done-1day' data-index='" +
+      i +
+      "'" +
+      (tasks[i][closestThreeDays[1]] ? " checked" : "") +
+      ">";
+    twoCell.innerHTML =
+      "<input type='checkbox' class='task-done-2day' data-index='" +
+      i +
+      "'" +
+      (tasks[i][closestThreeDays[2]] ? " checked" : "") +
+      ">";
+    deleteCell.innerHTML =
+      "<button class='delete-button' data-index='" + i + "'>Delete</button>";
+  }
+}
 
-        function convertDay(num) {
-            switch(num) {
-                case 0:
-                return "Sunday";
-                case 1:
-                return "Monday";
-                case 2:
-                return "Tuesday";
-                case 3:
-                return "Wednesday";
-                case 4:
-                return "Thursday";
-                case 5:
-                return "Friday";
-                case 6:
-                return "Saturday";
-                default:
-                return "Invalid input";
-            }
-        }
+// event listener for task done checkbox click - today
+document.addEventListener("change", function (event) {
+  if (event.target.matches(".task-done-today")) {
+    let index = event.target.dataset.index;
+    tasks[index][closestThreeDays[0]] = event.target.checked;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+});
 
-        //get dates to customize the table header
-        // const today = new Date().toLocaleDateString();
-        let fullDate = new Date(Date.now());
-        var pstDate = fullDate.toLocaleString("en-US", {
-            timeZone: "America/Los_Angeles"
-        })
-        pstDate = new Date(pstDate);
-        let month = pstDate.getMonth() + 1; //months from 1-12
-        let day = pstDate.getDate();
-        let today = month + "/" + day;
-        let oneDayAgo = month + "/" + (day-1);
-        let twoDayAgo = month + "/" + (day-2);
-		let weekDay = pstDate.getDay();
-        document.getElementById("today").innerHTML = convertDay(weekDay) + "</br>" + today;
-		if(weekDay <= 1){weekDay += 7;}
-        document.getElementById("oneDayAgo").innerHTML = convertDay(weekDay - 1) + "</br>" + oneDayAgo;
-        document.getElementById("twoDayAgo").innerHTML = convertDay(weekDay - 2) + "</br>" + twoDayAgo;
+// event listener for task done checkbox click - 1 day ago
+document.addEventListener("change", function (event) {
+  if (event.target.matches(".task-done-1day")) {
+    let index = event.target.dataset.index;
+    tasks[index][closestThreeDays[1]] = event.target.checked;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+});
 
+// event listener for task done checkbox click - 1 day ago
+document.addEventListener("change", function (event) {
+  if (event.target.matches(".task-done-2day")) {
+    let index = event.target.dataset.index;
+    tasks[index][closestThreeDays[2]] = event.target.checked;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+});
+
+// event listener for delete button click
+document.addEventListener("click", function (event) {
+  if (event.target.matches(".delete-button")) {
+    let index = event.target.dataset.index;
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    renderTasks();
+  }
+});
+
+// convert number into string for week days
+function convertDay(num) {
+  switch (num) {
+    case (0, 7):
+      return "Sunday";
+    case (1, 8):
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+    default:
+      return "Invalid input";
+  }
+}
+
+//get dates to customize the table header
+let weekDay = pstDate.getDay();
+if (weekDay <= 1) {
+  weekDay += 7;
+}
+document.getElementById("zeroDayAgo").innerHTML =
+  convertDay(weekDay) + "</br>" + closestThreeDays[0].slice(5);
+document.getElementById("oneDayAgo").innerHTML =
+  convertDay(weekDay - 1) + "</br>" + closestThreeDays[1].slice(5);
+document.getElementById("twoDayAgo").innerHTML =
+  convertDay(weekDay - 2) + "</br>" + closestThreeDays[2].slice(5);
+
+//code below are for changing dates and updating the form accordingly
+// Load the existing data array from local storage, or create a new one if none exists
+let dayData = JSON.parse(localStorage.getItem("dayData")) || [];
+if (dayData != [] && day != dayData[0]) {
+  console.log("dayData: " + dayData[0]);
+  console.log("rightNow: " + day);
+  newDay();
+}
+
+// Add today's data to the array
+dayData[0] = day;
+
+// Store the updated data array back in local storage
+localStorage.setItem("dayData", JSON.stringify(dayData));
+
+// if new day
+function newDay() {
+  // for every event
+  for (let i = 0; i < tasks.length; ++i) {
+    // for the tasks in every event
+    for (let j = 0; j < Object.keys(tasks[i]).length; ++j) {
+      tasks[i][closestThreeDays[1]] = tasks[i][closestThreeDays[0]];
+    }
+    tasks[i][closestThreeDays[0]] = false;
+  }
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  renderTasks();
+}
